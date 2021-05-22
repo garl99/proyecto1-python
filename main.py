b1035861bc28f051a9b75b09ef8b8999a6eaf7e4
Gabriel <garomerol.17@est.ucab.edu.ve>
2021-05-21 23:57:24 -0430

    Caso 2 y 3

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+#Funcion para recorrer lista de ingredientes y devolver 
+#cada ingrediente separados por coma
+def listarIngredientes(ingredientesAgregados):
+    ingredientes=''
+    for ingrediente in ingredientesAgregados:
+        ingredientes+=ingrediente.strip() +','
+    return ingredientes[:-1]
+
+#Funcion que pide el ingrediente y valida que sea correcto
+def inputIngrediente():
+    flag=True
+
+    while(flag):
+        ingredienteClave = str(input("Indique ingrediente (enter para terminar): "))
+        if ingredienteClave not in ingredientes.keys() and ingredienteClave!='':
+            print("=> Debe seleccionar un ingrediente correcto!!")
+        else:
+            break
+
+    return ingredienteClave
+
+#Funcion que pide el tamano y valida que sea correcto
+def inputTamano():
+    flag=True
+    while(flag):
+        tamano = str(input("Tamanos: Triple ( t ) Doble ( d ) Individual ( i ): "))
+        if tamano not in opcionesValidas or tamano=='':
+            print("=> Debe seleccionar un ingrediente correcto!!")
+        else:
+            break
+
+    return tamano 
+
 print("SANDWICHES UCAB")
 
-tamaños = {
+tamanos = {
     't': ['Triple', 580],
-    's': ['Doble', 430],
+    'd': ['Doble', 430],
     'i': ['Individual', 280]
 }
 
 ingredientes = {
     'ja': ['Jamón  ', 40], 
-    'ch': ['Champiñones', 35], 
+    'ch': ['Champinones', 35], 
     'pi': ['Pimentón', 30], 
     'dq': ['Doble Queso', 40], 
     'ac': ['Aceitunas', 57.5], 
@@ -18,6 +51,7 @@ ingredientes = {
 
 condicion = 's'
 contadorSand = 1
+total=0
 
 while condicion == 's' or condicion == 'S':
 
@@ -27,45 +61,48 @@ while condicion == 's' or condicion == 'S':
 
     opcionesValidas = 'tdi'
 
-    tamañoSand = str(input("Tamaños: Triple ( t ) Doble ( d ) Individual ( i ): "))
-
-    while tamañoSand not in opcionesValidas:
-        print("=> Debe seleccionar un tamaño correcto!!")
-        tamañoSand = str(input("Tamaños: Triple ( t ) Doble ( d ) Individual ( i ): "))
+    tamanoSand = inputTamano()
 
     print("Ingredientes:")
     for key in ingredientes:
         print(ingredientes[key][0], '\t(', key, ')')
 
-    ingrediente = str(input("Indique ingrediente (enter para terminar) "))
+    subtotal=int(tamanos.get(tamanoSand)[1])
+    cantidadIngrediente=0
+    ingredientesAgregados=[]
+    ingredienteClave = inputIngrediente()
 
-    while ingrediente not in ingredientes.keys() and ingrediente != '':
-        print("Error")
-        ingrediente = str(input("Indique ingrediente (enter para terminar) "))
+    while ingredienteClave in ingredientes.keys() or ingredienteClave=='':
+        
+        if cantidadIngrediente!=0:
+            ingredienteClave = inputIngrediente()
 
+        if ingredienteClave=='' and cantidadIngrediente==0:
+            ingredientesAgregados.append('Queso')
+            costoIngrediente = 0
+            subtotal += costoIngrediente
+            break
+        elif ingredienteClave==''and cantidadIngrediente>0:
+            break
+
+        datos = ingredientes.get(ingredienteClave)
+        ingredientesAgregados.append(datos[0])
+        subtotal+=datos[1]
+        
+        cantidadIngrediente+=1
     
-    datos = ingredientes.get(ingrediente)
-    
-    if ingrediente == '':
-        ingrediente = 'Queso'
-        costoIngrediente = 0
-    else:
-        ingrediente = datos[0]
-        costoIngrediente = datos[1]
+    tamano = str(tamanos.get(tamanoSand)[0])
 
-    
-    tamaño = str(tamaños.get(tamañoSand)[0])
+    print("Usted seleccionó un sandwich", tamano , "con", listarIngredientes(ingredientesAgregados))
+    print("Subtotal a pagar por un sandwich", tamano, ": ", subtotal)
 
-    subtotal = int(tamaños.get(tamañoSand)[1]) + costoIngrediente
-
-    print("Usted seleccionó un sandwich", tamaño , "con", ingrediente)
-
-
-    print("Subtotal a pagar por un sandwich", tamaño, ": ", subtotal)
+    total=total+subtotal
     
     condicion = str(input("Desea continuar [s/n]? "))
     
     if condicion == 's' or condicion == 'S':
         contadorSand += 1
-
-print("finish")
+    else:
+        print('El pedido tiene un total de',contadorSand,'sándwich(es) por un monto de ',total)
+        print('Gracias por su compra, regrese pronto')
+print("Fin del pedido")
