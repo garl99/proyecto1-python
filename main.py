cf840e3543e69589c2592f87da8cc4c5540a8513
Ricardo Pérez <ricardo.perez@jirehpro.co>
2021-05-23 14:25:06 -0400

    Funcionalidad de cupones de descuentos añadidas (funcionalidad extra)

--- main.py
+++ main.py
@@ -30,6 +30,36 @@ def inputTamano():
             break
     return tamano 
 
+# Función que agrega un cupon de descuento al pedido final
+def agregarCuponDescuento(precioTotal):
+    inputCupon = input("Introduzca cupón de descuento: ")
+    
+    if inputCupon in cupones.keys():
+        montoFinalDescuento = precioTotal * cupones[inputCupon] / 100
+        print(f"Cupón aplicado correctamente, tiene un {cupones[inputCupon]}% de descuento.\n")
+    else:
+        montoFinalDescuento = 0
+        print("El cupón que ha ingresado no es válido.\n")
+    return montoFinalDescuento        
+
+def aplicarCupon(contadorSand, total):
+    inputCupon = 's'
+        
+    while inputCupon == 's':
+        inputCupon = str(input("¿Desea aplicar un cupón de descuento? [s/n] "))
+        if inputCupon == 's' or inputCupon == 'S':
+            totalDescuento = total - agregarCuponDescuento(total)            
+            if(totalDescuento == total):
+                continue
+            else:
+                print('El pedido tiene un total de',contadorSand,'sándwich(es) por un monto (con descuento) de ',totalDescuento,"\n")
+                break            
+        else:
+            print('Gracias por su compra, regrese pronto')
+            break
+    return 0
+
+# Función que imprime 28 asteriscos por pantalla
 def printStars():
     return print("*" * 28)
 
@@ -37,12 +67,14 @@ printStars()
 print("*      SANDWICHES UCAB     *")
 printStars()
 
+# Tamaños de los sandwich con su precio base
 tamanos = {
     't': ['Triple', 580],
     'd': ['Doble', 430],
     'i': ['Individual', 280]
 }
 
+# Ingredientes con su precio adicional
 ingredientes = {
     'ja': ['Jamón  ', 40], 
     'ch': ['Champinones', 35], 
@@ -51,6 +83,15 @@ ingredientes = {
     'ac': ['Aceitunas', 57.5], 
     'pp': ['Pepperoni', 38.5],
     'sa': ['Salchichón', 62.5]
+}
+
+# Cupones con el porcentaje de descuento
+cupones = {
+    'XNSS10': 10,
+    'NHLE15': 15,
+    'GETI20': 20,
+    'SXFP30': 30,
+    'FARD50': 50
 }
 
 condicion = 's'
@@ -100,7 +141,7 @@ while condicion == 's' or condicion == 'S':
     print("Usted seleccionó un sandwich", tamano , "con", listarIngredientes(ingredientesAgregados))
     print("\nSubtotal a pagar por un sandwich", tamano, ": ", subtotal)
 
-    total = total+subtotal
+    total = total + subtotal
     
     printStars()
     condicion = str(input("Desea continuar [s/n]? "))
@@ -109,7 +150,7 @@ while condicion == 's' or condicion == 'S':
     if condicion == 's' or condicion == 'S':
         contadorSand += 1
     else:
-        print('El pedido tiene un total de',contadorSand,'sándwich(es) por un monto de ',total,"\n")
-        print('Gracias por su compra, regrese pronto')
+        print('El pedido tiene un total de',contadorSand,'sándwich(es) por un monto de ',total,"\n")   
+        aplicarCupon(contadorSand,total) # Funcionalidad cupones de descuento
         
 print("Fin del pedido")
